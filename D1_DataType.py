b7143a13d9d5fa70028fd90d0a5fa7336d0eb35a
N V Roshni <roshstar1999@gmail.com>
2025-03-26 08:44:57 +0530

    Create D1_DataType.py

---
+++ D1_DataType.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def dataTypeSize(self, str):
+        if(str=="Character"):
+            return 1
+        
+        elif (str=="Integer"):
+            return 4
+        
+        elif(str=="Long"):
+            return 8
+        
+        elif(str=="Float"):
+            return 4
+        
+        elif(str=="Double"):
+            return 8
